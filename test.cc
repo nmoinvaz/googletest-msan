a8f72567020bafec2e362c6ad5d4405688a03a3c
Nathan Moinvaziri <nathan@nathanm.com>
2022-02-02 20:46:48 -0800

    Try 2

--- test.cc
+++ test.cc
@@ -11,6 +11,11 @@ TEST(FooTest, BadFoo) {
   EXPECT_GT(uninitialized, 5);
 }
 
+TEST(FooTest, BadFoo2) {
+  char *m = (char *)malloc(10);
+  EXPECT_GT(m[0], 5);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
